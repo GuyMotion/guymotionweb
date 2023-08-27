511b0a213b64a2c9fd37cae254f8cfa6c6af05ce
Guy Motion <guymotion31@gmail.com>
2023-08-27 15:24:53 -0300

    Update V1.0.1

--- app/page.js
+++ app/page.js
@@ -1,11 +1,24 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      {/* Top Logo */}
+      <div className="absolute top-4 left-1/2 transform -translate-x-1/2">
+        <Image
+          src="/logo.png"  // Substitua pelo caminho da sua logo
+          alt="https://www.youtube.com/@GuyMotion/"
+          width={50}
+          height={50}
+        />
+      </div>
+
       {/* Animation Section */}
       <section className="mb-16 text-center">
-        <h2 className="text-3xl font-semibold mb-4">Animations</h2>
+        <Link href="/animations"> {/* Substitua pelo link correto */}
+          <a className="text-3xl font-semibold mb-4 hover:underline">Animations</a>
+        </Link>
         <p className="text-gray-600 mb-8">
           Explore a collection of captivating animations created by me.
         </p>
@@ -14,7 +27,9 @@ export default function Home() {
 
       {/* Drawing Section */}
       <section className="mb-16 text-center">
-        <h2 className="text-3xl font-semibold mb-4">Drawings</h2>
+        <Link href="/drawings"> {/* Substitua pelo link correto */}
+          <a className="text-3xl font-semibold mb-4 hover:underline">Drawings</a>
+        </Link>
         <p className="text-gray-600 mb-8">
           Discover a world of imagination through my artistic drawings.
         </p>
@@ -23,7 +38,9 @@ export default function Home() {
 
       {/* Games Section */}
       <section className="mb-16 text-center">
-        <h2 className="text-3xl font-semibold mb-4">Games</h2>
+        <Link href="/games"> {/* Substitua pelo link correto */}
+          <a className="text-3xl font-semibold mb-4 hover:underline">Games</a>
+        </Link>
         <p className="text-gray-600 mb-8">
           Dive into the realm of interactive entertainment with my games.
         </p>
@@ -32,36 +49,29 @@ export default function Home() {
 
       {/* Music Section */}
       <section className="mb-16 text-center">
-        <h2 className="text-3xl font-semibold mb-4">Music</h2>
+        <Link href="/music"> {/* Substitua pelo link correto */}
+          <a className="text-3xl font-semibold mb-4 hover:underline">Music</a>
+        </Link>
         <p className="text-gray-600 mb-8">
           Immerse yourself in melodies and harmonies of my musical compositions.
         </p>
         {/* Display music examples here */}
       </section>
 
-      {/* Your Next.js logo */}
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/logo.png"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+      {/* Animated GIF */}
+      <div className="relative flex place-items-center h-48 w-full items-end justify-center">
+        <img
+          src="/your-animated-logo.gif"  // Substitua pelo caminho do seu GIF animado
+          alt="Animated Logo"
+          className="dark:invert"
+          width={100}
+          height={100}
         />
       </div>
 
       {/* Footer */}
       <footer className="flex justify-center mt-16 text-gray-500">
-        <p>
-          Created by {' '}
-          <a
-            href="https://github.com/GuyMotion"
-            className="text-blue-500 hover:underline"
-          >
-            David Dias
-          </a>
-        </p>
+        {/* ... Footer content ... */}
       </footer>
     </main>
   )
